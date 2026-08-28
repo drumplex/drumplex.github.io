c2b5afdc84e241b72bb2895dcbfa846430399f1e
drumplex <drumplexx@gmail.com>
2026-08-28 16:01:21 -0700

    Create AudioEngine.js

---
+++ ChronarchAuthority/src/world/AudioEngine.js
@@ -0,0 +1,44 @@
+export class AudioEngine {
+  constructor() {
+    this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+    this.masterGain = this.ctx.createGain();
+    this.masterGain.connect(this.ctx.destination);
+    this.masterGain.gain.value = 0.3;
+  }
+
+  playProceduralHum() {
+    if (this.ctx.state === 'suspended') {
+      this.ctx.resume();
+    }
+    // Synthesis ambient electrical hum oscillator
+    const osc = this.ctx.createOscillator();
+    const gain = this.ctx.createGain();
+    
+    osc.type = 'sawtooth';
+    osc.frequency.setValueAtTime(55, this.ctx.currentTime); // Low A hum
+    
+    gain.gain.setValueAtTime(0.05, this.ctx.currentTime);
+    osc.connect(gain);
+    gain.connect(this.masterGain);
+    
+    osc.start();
+  }
+
+  playAlertSound() {
+    const osc = this.ctx.createOscillator();
+    const gain = this.ctx.createGain();
+    
+    osc.type = 'square';
+    osc.frequency.setValueAtTime(440, this.ctx.currentTime);
+    osc.frequency.exponentialRampToValueAtTime(880, this.ctx.currentTime + 0.3);
+    
+    gain.gain.setValueAtTime(0.2, this.ctx.currentTime);
+    gain.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + 0.3);
+    
+    osc.connect(gain);
+    gain.connect(this.masterGain);
+    
+    osc.start();
+    osc.stop(this.ctx.currentTime + 0.3);
+  }
+}
